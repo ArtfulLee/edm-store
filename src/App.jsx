f7f633fef1fab77fe4c49ef939ad44972ef8ebe1
ArtfulLee <aastepsb@yandex.ru>
2024-06-23 21:54:36 +0300

    feat: add component Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import Card from "./components/ui/Card/Card";
 import libraryOfMusic from "./temp/data";
 
 const App = () => {
-  console.log(libraryOfMusic)
+  console.log(libraryOfMusic);
   return (
     <>
-      <div className="container mx-auto p-4">
-        <div className="flex flex-wrap justify-between">
-          {!!libraryOfMusic &&
-            libraryOfMusic.map((track) => {
-              <Card details={track} />;
-            })}
+      <div className=" bg-neutral-900 h-screen w-screen">
+        <div className="container mx-auto p-4">
+          {/* Временная секция */}
+          <section className="max-w-5xl mx-auto">
+            <div className="flex flex-wrap justify-between">
+              {!!libraryOfMusic &&
+                libraryOfMusic.map((audioDetails) => (
+                  <Card key={audioDetails.id} details={audioDetails} />
+                ))}
+            </div>
+          </section>
         </div>
       </div>
     </>
--- src/temp/data.js
+++ src/temp/data.js
@@ -9,6 +9,29 @@ const libraryOfMusic = [
     price: "3$",
     trackName: "Bronx Posse",
     artist: "Skool Of Thought, Stanton Warriors",
+    genre: "Breaks",
+  },
+  {
+    id: "1",
+    imgSrc:
+      "https://geo-media.beatport.com/image_size/1400x1400/6b9a02d4-709f-4074-87ba-03f5abcc4a09.jpg",
+    audioSrs:
+      "https://geo-samples.beatport.com/track/0ec7afb9-9566-4ffd-a8e6-eed24d0b7880.LOFI.mp3",
+    price: "3$",
+    trackName: "Get Ready",
+    artist: "Aggresivnes",
+    genre: "Breaks",
+  },
+  {
+    id: "2",
+    imgSrc:
+      "https://geo-media.beatport.com/image_size/1400x1400/655864c7-99b1-4430-8c02-4ed4da791c03.jpg",
+    audioSrs:
+      "https://geo-samples.beatport.com/track/25f5e5fe-f607-4ca7-a13d-57d4ec697b0f.LOFI.mp3",
+    price: "3$",
+    trackName: "Gold Dust",
+    artist: "Krafty Kuts, DJ Fresh",
+    genre: "Breaks",
   },
 ];
 
