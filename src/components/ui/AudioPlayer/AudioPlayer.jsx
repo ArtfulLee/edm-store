21e616f0138680b2ac02e14fc0ee036c9f24960b
ArtfulLee <aastepsb@yandex.ru>
2024-06-30 17:14:08 +0300

    feat: add component AudioPlayer WIP

--- src/components/ui/AudioPlayer/AudioPlayer.jsx
+++ src/components/ui/AudioPlayer/AudioPlayer.jsx
@@ -1,8 +1,11 @@
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+
 const AudioPlayer = () => {
   return (
     <>
       <div className="audio-player">
         <div className="audio-player__inner">
+          <PlayCircleIcon className="text-pink-400" />
         </div>
       </div>
     </>
