9dd0b1b27681aebc2e5ffca880b63d33497bc83c
ArtfulLee <aastepsb@yandex.ru>
2024-07-17 15:22:31 +0300

    feat: add text constants

---
+++ src/constants/texts.js
@@ -0,0 +1,14 @@
+/**
+ * Глобальный объект AUDIO_CARD__TEXTS
+ */
+export const AUDIO_CARD__TEXTS = {
+  idOfTrack: "Идентификатор карточки",
+  imgSrc: "Путь к изображению",
+  audioSrs: "Путь к аудио",
+  price: "Цена аудио",
+  nameOfTrack: "Название композиции",
+  artist: "Имена артистов",
+  genre: "Жанр",
+  isFavorite: "Добавлено в избранные или нет",
+  numberOfSales: "Количество продаж",
+};
