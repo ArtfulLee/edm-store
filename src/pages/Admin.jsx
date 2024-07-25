b5e1a694d054b571196f27f6005ef39b5db46609
ArtfulLee <aastepsb@yandex.ru>
2024-07-25 14:34:39 +0300

    feat: add Admin page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+const Admin = () => {
+  return (
+    <>
+      <div className="container mx-auto my-4">
+        <h1 className="text-2xl font-bold text-neutral-50">Admin Page</h1>
+        <p className="text-neutral-50">Welcome to the Admin page!</p>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
