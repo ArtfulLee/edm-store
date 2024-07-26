d2930300892ef86bface6f02faf61dfa0caee43b
ArtfulLee <aastepsb@yandex.ru>
2024-07-26 13:24:14 +0300

    feat: update global text constants

--- src/constants/errorTexts.js
+++ src/constants/errorTexts.js
@@ -1,3 +1,5 @@
 export const ERROR_TEXTS = {
-  errorFetch: "Ошибка при запросе",
+  errorFetch: "Ошибка при запросе на сервер",
+  errorDataAuth: "Invalid username or password",
+  errorSingUpNewUser: "Ошибка при регистрации пользователя:"
 };
