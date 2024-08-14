3a5e6ac8c845ab5136328d92ea280505e8cb03a2
ArtfulLee <aastepsb@yandex.ru>
2024-08-14 19:00:33 +0300

    feat: add constant for alert texts

---
+++ src/constants/alertTexts.js
@@ -0,0 +1,14 @@
+export const ALERT__TEXTS = {
+  editItem: {
+    title: "Successfully edited",
+    subtitle: "The item has been successfully edited",
+  },
+  addItem: {
+    title: "Successfully added",
+    subtitle: "The item has been successfully added",
+  },
+  deleteItem: {
+    title: "Successfully deleted",
+    subtitle: "The item has been successfully deleted",
+  },
+};
