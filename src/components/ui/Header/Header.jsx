1a293d8a8f43ef2cf3595b0bf44c8c391be75499
ArtfulLee <aastepsb@yandex.ru>
2024-09-01 12:44:45 +0300

    fix: correct logout and redirect on home page

--- src/components/ui/Header/Header.jsx
+++ src/components/ui/Header/Header.jsx
@@ -1,6 +1,6 @@
 // React
 import { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 // components
 import useForm from "../../../hooks/useForm";
@@ -204,13 +204,15 @@ const Header = () => {
                   </button>
                 </>
               ) : (
-                <button
-                  type="button"
-                  className="border-2 border-emerald-400 bg-emerald-400 hover:border-emerald-300 hover:bg-emerald-300 text-neutral-900 font-semibold p-1 transition duration-100"
-                  onClick={onLogout}
-                >
-                  Logout
-                </button>
+                <Link to="/">
+                  <button
+                    type="button"
+                    className="border-2 border-emerald-400 bg-emerald-400 hover:border-emerald-300 hover:bg-emerald-300 text-neutral-900 font-semibold p-1 transition duration-100"
+                    onClick={onLogout}
+                  >
+                    Logout
+                  </button>
+                </Link>
               )}
             </div>
             {showRegisterModal && (
